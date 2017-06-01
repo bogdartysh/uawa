2dc2e8190ea06b75174bfe0c2471c704048ba9de
Bogdan Artyushenko <ext-bogdan.artyushenko@here.com>
2017-06-01 13:57:51 +0300

    додано запис

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/bogdartysh/uawa"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/data.json "0.2.6"]])
+  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/data.json "0.2.6"]]
+  :aot :all
+)
 
