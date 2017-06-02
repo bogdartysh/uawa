66739a71aa748e2685e27e2609c447932c5f2231
Bogdan Artyushenko <ext-bogdan.artyushenko@here.com>
2017-06-02 19:17:32 +0300

    додано другий семпл

---
+++ samples/neo4j_helper/project.clj
@@ -0,0 +1,5 @@
+(defproject app "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"  
+  :dependencies [[org.clojure/clojure "1.8.0"], [clj-http "2.3.0"], [uawa/uawa "0.1.0-SNAPSHOT"]]
+  :main app.core
+)
